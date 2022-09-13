13c4e232255f9864b2335cc30a53a708cb88ad92
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-09-13 10:07:54 +0530

    Time: 3 ms (68.09%), Space: 67.1 MB (76.93%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,16 @@
+class Solution { 
+public boolean canJump(int[] nums) { 
+int n = nums.length; 
+if(n==1) 
+return true; 
+int max = 0; // max is maximum jump 
+for(int i=0;i<n-1 && max>=i;i++) 
+{ 
+if(max<i + nums[i]) 
+max = i + nums[i]; 
+if(max>=n-1) 
+return true; 
+} 
+return false; 
+} 
+} 
