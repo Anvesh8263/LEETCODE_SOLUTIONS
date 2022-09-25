f089f48bcb14c2d7a6742ca6515be845c580b697
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-09-26 00:49:52 +0530

    Time: 1 ms (99.90%), Space: 57.7 MB (27.01%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -16,9 +16,9 @@ public void reverseString(char[] s) {
 int right = s.length-1;
 int left=0;
 while(left<right){
-char temp = s[right];
-s[right--] = s[left];
-s[left++] = temp;
+char temp = s[left];
+s[left++] = s[right];
+s[right--] = temp;
 }
 }
 }
