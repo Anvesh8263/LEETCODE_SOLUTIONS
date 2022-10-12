547818f0c86e6608436f0717369a03922072ca99
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2022-10-12 10:38:22 +0530

    Time: 1 ms (49.71%), Space: 40.4 MB (91.81%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -4,9 +4,21 @@ class Solution {
       if(root==null){
           return array;
       }  
-      array.add(root.val);
-      preorderTraversal(root.left);
-       preorderTraversal(root.right);
-       return array;
+      // array.add(root.val);
+      // preorderTraversal(root.left);
+      //  preorderTraversal(root.right);
+      //  return array;
+        Stack<TreeNode>stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+        TreeNode temp = stack.pop();
+            array.add(temp.val);
+            if(temp.right!=null){
+           stack.push(temp.right);}
+            if(temp.left!=null){
+           stack.push(temp.left);}
+            }
+           return array;
+        }
+
     }
-}
