290642d14e01e484ab52ab023f1a96d3e34888b5
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2023-01-06 12:23:56 +0530

    Time: 11 ms (98.34%), Space: 82.7 MB (5.21%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,17 +1,48 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
         //O(nlogn) approch;
-        Arrays.sort(costs);
+        // Arrays.sort(costs);
+        // int count=0;
+        // for(int i=0;i<costs.length;i++){
+        //     if(costs[i]<=coins){
+        //         count++;
+        //         coins-=costs[i];
+        //     }
+        //     else{
+        //         return count;
+        //     }
+        // }
+        // return count;
+        
         int count=0;
+        int max=0;
         for(int i=0;i<costs.length;i++){
-            if(costs[i]<=coins){
-                count++;
-                coins-=costs[i];
+            if(max<costs[i]){
+                max=costs[i];
             }
-            else{
+        }
+        int freq[]=new int[max+1];
+        for(int i=0;i<costs.length;i++){
+            freq[costs[i]]++;
+        }
+        for(int i=1;i<max+1;i++){
+            if(freq[i]!=0 && i*freq[i]<=coins){
+                count+=freq[i];
+                coins-=i*freq[i];
+            }
+            else if(freq[i]!=0 && coins>=i){ 
+                for(int j=1;j<=freq[i];j++){
+                    if(coins>=i){
+                        count++;
+                        coins-=i;
+                    }
+                }
+            }
+            else if(coins<i){
                 return count;
             }
         }
         return count;
+        
     }
 }
