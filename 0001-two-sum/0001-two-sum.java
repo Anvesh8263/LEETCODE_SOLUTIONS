4ae2b07a970ed2e598d12107dd1f2cef7eecd3a6
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2023-01-11 10:59:22 +0530

    Time: 64 ms (39.26%), Space: 42.7 MB (63.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -26,7 +26,19 @@ ans[0]=i;
 }
              return ans;
              }
-             }
-        
+         }
+//         int[] result = new int[2];
+//     Map map = new HashMap();
+//     for(int i=0;i<numbers.length;i++){
+// if(!map.containsKey(target-numbers[i])){
+//     map.put(numbers[i],i);
+// }else{
+//     result[1]=i;
+//     result[0]=map.get(target-numbers[i]);
+// }
+//     }
+//     return result;
+// }
+// }
 
        
