bdc5530d9dabfd9bffe840df62495f107dc85358
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2023-01-23 10:27:39 +0530

    Create Arithmetic_Operators.php

---
+++ Arithmetic_Operators.php
@@ -0,0 +1,17 @@
+<?php
+$a = 95;
+$b = 5;
+
+echo  "Result of a + b is " . ($a+$b);
+echo "<br>";
+echo  "Result of a - b is " . ($a-$b);
+echo "<br>";
+echo  "Result of a * b is " . ($a*$b);
+echo "<br>";
+echo  "Result of a / b is " . ($a/$b);
+echo "<br>";
+echo  "Result of a**b is " . ($a**$b);
+echo "<br>";
+echo  "Result of a%b is " . ($a%$b);
+echo "<br>";
+?>
