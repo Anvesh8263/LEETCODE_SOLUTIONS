82b8b75c4fa1cdf47a74a2de1a611385b702d262
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2023-06-12 11:31:55 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (57.82%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,9 +1,12 @@
 class Solution {
     public boolean divisorGame(int n) {
-         if ( n == 1){
-              return false;
-      }else{
-              return !divisorGame(n -1);
-      }
+//          if ( n == 1){
+//               return false;
+//       }else{
+//               return !divisorGame(n -1);
+//       }
+//     }
+// }
+        return n%2==0? true:false;
     }
 }
